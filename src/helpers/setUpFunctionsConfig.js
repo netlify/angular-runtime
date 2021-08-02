df106ed511c7a98c353dbf4acb24b276dcbb0eb1
lindsaylevine <57350178+lindsaylevine@users.noreply.github.com>
2021-08-02 15:30:14 -0400

    fix: stylesheet issue (#9)
    * fix: stylesheet issue
    
    * chore: handle including multiple src/stylesheets

--- src/helpers/setUpFunctionsConfig.js
+++ src/helpers/setUpFunctionsConfig.js
@@ -1,8 +1,14 @@
+const getAngularJson = require('./getAngularJson')
+
 // Modify [functions] of netlify config
-const setUpFunctionsConfig = function ({ netlifyConfig, PUBLISH_DIR }) {
+const setUpFunctionsConfig = function ({ angularJson, netlifyConfig, PUBLISH_DIR }) {
   netlifyConfig.functions.node_bundler = 'esbuild'
 
-  const includedDist = [`${PUBLISH_DIR}/index.html`]
+  const includeStyles = angularJson.projects[angularJson.defaultProject].architect.build.options.styles.map(
+    (s) => `${PUBLISH_DIR}/${s.replace('src/', '')}`,
+  )
+
+  const includedDist = [`${PUBLISH_DIR}/index.html`, `${PUBLISH_DIR}/styles.css`, ...includeStyles]
   if (Array.isArray(netlifyConfig.functions.included_files)) {
     netlifyConfig.functions.included_files.push(...includedDist)
   } else {
