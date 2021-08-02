552677866346b3026e9bcfab1488b16575d972f0
lindsaylevine <57350178+lindsaylevine@users.noreply.github.com>
2021-08-02 15:29:20 -0400

    fix: upgrade serverless express engine (#8)

--- src/helpers/getDynamicTemplates.js
+++ src/helpers/getDynamicTemplates.js
@@ -1,23 +1,24 @@
 const getAngularBuilder = ({ functionServerPath }) => `
   const { builder } = require('@netlify/functions')
-  const awsServerlessExpress = require('aws-serverless-express')
-  const awsServerlessExpressMiddleware = require('aws-serverless-express/middleware')
-
-  // eslint-disable-next-line node/no-missing-require, node/no-unpublished-require
+  const serverlessExpress = require('@vendia/serverless-express')
 
   const server = require('${functionServerPath}')
 
-  // makes event and context available to app
-  server.app.use(awsServerlessExpressMiddleware.eventContext())
-
-  const serverProxy = awsServerlessExpress.createServer(server.app)
-
-  const handler = (event, context) => awsServerlessExpress.proxy(serverProxy, event, context, 'PROMISE').promise
+  const handler = async (event, context) => {
+    try {
+      const serverlessHandler = await serverlessExpress({ app: server.app, eventSourceName: 'AWS_API_GATEWAY_V1' })
+      const response = await serverlessHandler(event, context)
+      return response
+    } catch (e) {
+      return {
+        statusCode: 404
+      }
+    }
+  }
 
   exports.handler = builder(handler)
 `
 
-// TO-DO: improve error handling
 const getServerlessTs = ({ projectName, siteRoot }) => `
   import 'zone.js/dist/zone-node'
 
