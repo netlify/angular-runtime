81b2ce0fd0022311a6a4bc6b434905f3ce26cbef
Matt Kane <matt.kane@netlify.com>
2024-05-22 12:02:17 +0100

    feat: support Angular 18

--- src/helpers/validateAngularVersion.js
+++ src/helpers/validateAngularVersion.js
@@ -18,8 +18,8 @@ const validateAngularVersion = async function (root) {
   }
 
   const { version } = await readJSON(packagePath)
-  if (!satisfies(version, '^17.0.0-rc', { includePrerelease: true })) {
-    console.warn(`This site does not seem to be using Angular 17.`)
+  if (!satisfies(version, '>=17.0.0-rc', { includePrerelease: true })) {
+    console.warn(`This site does not seem to be using Angular 17 or later. Found: ${version}.`)
     return false
   }
 
