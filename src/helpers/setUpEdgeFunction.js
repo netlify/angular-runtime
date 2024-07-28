8f2429576005d42e248e5b16d3b1dde23718a039
Hrishikesh Kokate <hrishi5200@gmail.com>
2024-07-28 23:00:08 +0530

    fix: crypto missing in polyfill
    Reported by customer: https://answers.netlify.com/t/122805

--- src/helpers/setUpEdgeFunction.js
+++ src/helpers/setUpEdgeFunction.js
@@ -89,7 +89,9 @@ const setUpEdgeFunction = async ({ angularJson, constants, failBuild }) => {
   // we ensure this is initialised before any other module
   const polyfills = `
   import process from "node:process"
+  import crypto from "node:crypto"
 
+  globalThis.crypto = crypto
   globalThis.process = process
   globalThis.DenoEvent = globalThis.Event // storing this for fixup-event.mjs
   `
