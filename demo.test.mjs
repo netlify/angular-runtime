27c79b638bb56646a8e16c32c75a6ec6e022a720
pieh <misiek.piechowiak@gmail.com>
2024-11-19 18:23:23 +0100

    chore: bump angular@19 from rc to stable

--- demo.test.mjs
+++ demo.test.mjs
@@ -9,7 +9,7 @@ test('edge function config', async () => {
     '/favicon.ico',
     '/heroes/index.html',
     '/index.csr.html',
-    '/main-KVCR6MBP.js',
+    '/main-OXU4ZHAA.js',
     '/polyfills-FFHMD2TL.js',
     '/styles-5INURTSO.css',
     '/heroes',
