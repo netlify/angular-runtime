94e9ff1b011037791172096591966c537174ebae
Michal Piechowiak <misiek.piechowiak@gmail.com>
2025-02-13 12:08:24 +0100

    chore: upgrade angular@19 versions (#250)
    * chore: upgrade angular@19 versions
    
    * test: mask file hashes to not have to update assertion everytime dependencies are updated

--- demo.test.mjs
+++ demo.test.mjs
@@ -4,14 +4,16 @@ import { test } from 'node:test'
 test('edge function config', async () => {
   const { config } = await import('./demo/.netlify/edge-functions/angular-ssr/angular-ssr.mjs')
 
-  assert.deepEqual(config.excludedPath, [
+  const excludedPathsWithMaskedHashes = config.excludedPath.map((path) => path.replace(/-[A-Z\d]{8}\./, '-HASHHASH.'))
+
+  assert.deepEqual(excludedPathsWithMaskedHashes, [
     '/.netlify/*',
     '/favicon.ico',
     '/heroes/index.html',
     '/index.csr.html',
-    '/main-KVCR6MBP.js',
-    '/polyfills-FFHMD2TL.js',
-    '/styles-5INURTSO.css',
+    '/main-HASHHASH.js',
+    '/polyfills-HASHHASH.js',
+    '/styles-HASHHASH.css',
     '/heroes',
   ])
 })
