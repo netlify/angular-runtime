d1eb0deb77f9820bb733dae6eaeca47dad575557
Michal Piechowiak <misiek.piechowiak@gmail.com>
2025-04-11 07:29:24 +0200

    test: upgrade demo to 20-next

--- demo/src/app/app.config.server.ts
+++ demo/src/app/app.config.server.ts
@@ -1,6 +1,5 @@
+import { provideServerRendering, provideServerRoutesConfig } from '@angular/ssr';
 import { mergeApplicationConfig, ApplicationConfig } from '@angular/core'
-import { provideServerRendering } from '@angular/platform-server'
-import { provideServerRoutesConfig } from '@angular/ssr'
 import { appConfig } from './app.config'
 import { serverRoutes } from './app.routes.server'
 
