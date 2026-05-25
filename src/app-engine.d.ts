36a92f36fb7715a97de843a8f916a9569a78cee2
Hrishikesh Kokate <hrishi5200@gmail.com>
2026-05-25 23:24:46 +0530

    allow specifying own hosts

--- src/app-engine.d.ts
+++ src/app-engine.d.ts
@@ -1,5 +1,5 @@
 import type { Context } from '@netlify/edge-functions'
 
-export declare function getAllowedHosts(): string[]
+export declare function getAllowedHosts(config?: { allowedHosts?: string[]; injectDefaults?: boolean }): string[]
 export declare function getContext(): Context | undefined
 export declare function getTrustProxyHeaders(): string[]
